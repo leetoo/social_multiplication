9a16c41460a5f319410f5f6836736599e9906ef9
tmf1 <timflorian1@gmail.com>
2018-04-09 15:20:38 +0300

    add domain : User , MultiplicationResultAttempt add corresponding controllers MultiplicationController and MultiplicationResultAttemptController and etc

--- social-multiplication/src/main/java/microservices/book/multiplication/domain/Multiplication.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -1,31 +1,24 @@
 package microservices.book.multiplication.domain;
 
-public class Multiplication {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-  private int factorA;
-  private int factorB;
-  // The result of the operation A * B
-  private int result;
-  public Multiplication(int factorA, int factorB) {
-    this.factorA = factorA;
-    this.factorB = factorB;
-    this.result = factorA * factorB;
-  }
-  public int getFactorA() {
-    return factorA;
-  }
-  public int getFactorB() {
-    return factorB;
-  }
-  public int getResult() {
-    return result;
-  }
-  @Override
-  public String toString() {
-    return "Multiplication{" +
-      "factorA=" + factorA +
-      ", factorB=" + factorB +
-      ", result(A*B)=" + result +
-      '}';
+/**
+ * This class represents a Multiplication (a * b).
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
+  // Both factors
+  private final int factorA;
+  private final int factorB;
+
+  // Empty constructor for JSON (de)serialization
+  Multiplication() {
+    this(0, 0);
   }
 }
--- social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
@@ -1,16 +1,16 @@
 package microservices.book.multiplication.service;
 
-import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
-public class RandomGeneratorServiceImpl implements RandomGeneratorService {
-  private final static int MINIMUM_FACTOR = 11;
-  private final static int MAXIMUM_FACTOR = 99;
+final class RandomGeneratorServiceImpl implements RandomGeneratorService {
 
-  @Override
-  public int generateRandomFactor() {
-    return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
-  }
+    final static int MINIMUM_FACTOR = 11;
+    final static int MAXIMUM_FACTOR = 99;
+
+    @Override
+    public int generateRandomFactor() {
+        return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+    }
 }
