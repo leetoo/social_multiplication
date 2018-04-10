3c6cc62be24d85d5f1d309449ae01a4dd5682c25
Max Rockatansky <denisigoshev@gmail.com>
2018-04-10 14:45:01 +0300

    start new gamification microservice ( as a implementation of user story 3 )

---
+++ gamification/src/main/java/microservices/book/gamification/domain/GameStats.java
@@ -0,0 +1,45 @@
+package microservices.book.gamification.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class GameStats {
+
+    private final Long userId;
+    private final int score;
+    private final List<Badge> badges;
+
+    // Empty constructor for JSON / JPA
+    public GameStats() {
+        this.userId = 0L;
+        this.score = 0;
+        this.badges = new ArrayList<>();
+    }
+
+    /**
+     * Factory method to build an empty instance (zero points and no badges)
+     * @param userId the user's id
+     * @return a {@link GameStats} object with zero score and no badges
+     */
+    public static GameStats emptyStats(final Long userId) {
+        return new GameStats(userId, 0, Collections.emptyList());
+    }
+
+    /**
+     * @return an unmodifiable view of the badge cards list
+     */
+    public List<Badge> getBadges() {
+        return Collections.unmodifiableList(badges);
+    }
+}
