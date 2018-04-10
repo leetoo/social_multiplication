a4b23f1ad2e334b377de7ca85edb264734ffd7af
Max Rockatansky <denisigoshev@gmail.com>
2018-04-10 14:49:00 +0300

    gamification : added repository layout to handle crud operations

---
+++ gamification/src/main/java/microservices/book/gamification/repository/ScoreCardRepository.java
@@ -0,0 +1,26 @@
+package microservices.book.gamification.repository;
+
+import microservices.book.gamification.domain.LeaderBoardRow;
+import microservices.book.gamification.domain.ScoreCard;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+
+public interface ScoreCardRepository extends CrudRepository<ScoreCard, Long> {
+
+
+    @Query("SELECT SUM(s.score) FROM microservices.book.gamification.domain.ScoreCard s WHERE s.userId = :userId GROUP BY s.userId")
+    int getTotalScoreForUser(@Param("userId") final Long userId);
+
+
+    @Query("SELECT NEW microservices.book.gamification.domain.LeaderBoardRow(s.userId, SUM(s.score)) " +
+            "FROM microservices.book.gamification.domain.ScoreCard s " +
+            "GROUP BY s.userId ORDER BY SUM(s.score) DESC")
+    List<LeaderBoardRow> findFirst10();
+
+
+    List<ScoreCard> findByUserIdOrderByScoreTimestampDesc(final Long userId);
+}
